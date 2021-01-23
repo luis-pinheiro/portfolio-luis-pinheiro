c76567cec8e87171da54db3feb1a665d45dc6ce8
Luis Pinheiro <profitcreations@live.com>
2021-01-23 18:02:21 +0000

    changed style of switch button

--- components/Switch2.js
+++ components/Switch2.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Switch = ({ isToggled, onToggle }) => {
   return (
-    <div className="fixed flex rounded-full z-60 w-11 h-7 top-1/4 lg:top-5 right-5">
+    <div className="fixed flex rounded-full z-60 w-11 h-7 top-28 right-1">
       {/* Toggle Button */}
       <label htmlFor="toogleA" className="flex items-center justify-center rounded-full cursor-pointer">
         {/* toggle  */}
@@ -10,12 +10,8 @@ const Switch = ({ isToggled, onToggle }) => {
           {/* input */}
           <input id="toogleA" type="checkbox" className="hidden" checked={isToggled} onChange={onToggle} />
           {/* line */}
-          <div className="w-10 h-10 bg-white rounded-full ">
-            {isToggled ? (
-              <i className="absolute text-yellow-500 fas fa-sun left-2 top-2"></i>
-            ) : (
-              <i className="absolute fas fa-moon right-2 top-2"></i>
-            )}
+          <div className="flex items-center justify-center w-10 h-10 text-lg rounded-full text-nord1 bg-nord6 dark:hover:text-shadow elevation-1 dark:hover:text-nord6 hover:bg-nord7 hover:elevation-2">
+            {isToggled ? <i className="absolute fas fa-sun"></i> : <i className="absolute fas fa-moon"></i>}
           </div>
           {/* dot */}
           {/* <div className="absolute w-6 h-6 bg-blue-600 rounded-full shadow hover:elevation-7 inset-y-1 left-1 toggle__dot"></div> */}

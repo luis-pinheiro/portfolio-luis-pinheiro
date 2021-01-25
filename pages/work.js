318f4a9fc3ab50d5d84a6c335984c25fab54c7f0
Luis Pinheiro <profitcreations@live.com>
2021-01-25 20:16:06 +0000

    fixed hedaer position in contact

--- pages/work.js
+++ pages/work.js
@@ -6,63 +6,62 @@ import AllPagesLayout from '../layouts/allPagesLayout';
 
 const work = () => {
   return (
-    // <CardFolio classes="w-screen h-screen">
-    <CardFolio classes="">
-      <div>
-        <CardHeader icon="fas fa-briefcase" title="Work" />
-      </div>
+    <div>
+      <CardHeader icon="fas fa-briefcase" title="Work" />
 
-      <div className="container grid w-full h-full px-5 mx-auto mb-5 gap-7 lg:pt-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 auto-rows-auto">
-        <CardWork
-          img="/images/portfolio/profitcreations.png"
-          title="My Old Portfolio"
-          description="FrontEnd"
-          tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY GULP NPM SASS"
-          link="https://luis-pinheiro.github.io/"
-          linkCode="https://github.com/luis-pinheiro/luis-pinheiro.github.io"
-        />
-        <CardWork
-          img="/images/portfolio/latuta.png"
-          title="Latuta"
-          description="WordPress Theme"
-          tags="HTML CSS JAVASCRIPT MATERIALIZECSS JQUERY WORDPRESS GULP NPM SASS"
-          link=""
-          linkCode="https://github.com/luis-pinheiro/Latuta"
-        />
-        <CardWork
-          img="/images/portfolio/nc-customs.png"
-          title="NC-Customs"
-          description="WordPress Theme"
-          tags="HTML CSS JAVASCRIPT MATERIALIZECSS JQUERY WORDPRESS GULP NPM SASS wd_s"
-          link=""
-          linkCode="https://bitbucket.org/ProfitCreations/nccustoms-wp/src/master/"
-        />
-        <CardWork
-          img="/images/portfolio/Trendytv.png"
-          title="TrendyTV"
-          description="Website"
-          tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY LESS"
-          link=""
-          linkCode=""
-        />
-        <CardWork
-          img="/images/portfolio/vastgoedjunior.png"
-          title="VastGoedJunior"
-          description="Website"
-          tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY JOOMLA LESS"
-          link=""
-          linkCode=""
-        />
-        <CardWork
-          img="/images/portfolio/teetopup.png"
-          title="TeeTopUp"
-          description="FrontEnd"
-          tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY GULP NPM COFFEESCRIPT SASS"
-          link=""
-          linkCode=""
-        />
-      </div>
-    </CardFolio>
+      <CardFolio classes="">
+        <div className="container grid w-full h-full px-5 mx-auto mb-5 gap-7 lg:pt-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 auto-rows-auto">
+          <CardWork
+            img="/images/portfolio/profitcreations.png"
+            title="My Old Portfolio"
+            description="FrontEnd"
+            tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY GULP NPM SASS"
+            link="https://luis-pinheiro.github.io/"
+            linkCode="https://github.com/luis-pinheiro/luis-pinheiro.github.io"
+          />
+          <CardWork
+            img="/images/portfolio/latuta.png"
+            title="Latuta"
+            description="WordPress Theme"
+            tags="HTML CSS JAVASCRIPT MATERIALIZECSS JQUERY WORDPRESS GULP NPM SASS"
+            link=""
+            linkCode="https://github.com/luis-pinheiro/Latuta"
+          />
+          <CardWork
+            img="/images/portfolio/nc-customs.png"
+            title="NC-Customs"
+            description="WordPress Theme"
+            tags="HTML CSS JAVASCRIPT MATERIALIZECSS JQUERY WORDPRESS GULP NPM SASS wd_s"
+            link=""
+            linkCode="https://bitbucket.org/ProfitCreations/nccustoms-wp/src/master/"
+          />
+          <CardWork
+            img="/images/portfolio/Trendytv.png"
+            title="TrendyTV"
+            description="Website"
+            tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY LESS"
+            link=""
+            linkCode=""
+          />
+          <CardWork
+            img="/images/portfolio/vastgoedjunior.png"
+            title="VastGoedJunior"
+            description="Website"
+            tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY JOOMLA LESS"
+            link=""
+            linkCode=""
+          />
+          <CardWork
+            img="/images/portfolio/teetopup.png"
+            title="TeeTopUp"
+            description="FrontEnd"
+            tags="HTML CSS JAVASCRIPT BOOTSTRAP JQUERY GULP NPM COFFEESCRIPT SASS"
+            link=""
+            linkCode=""
+          />
+        </div>
+      </CardFolio>
+    </div>
   );
 };
 

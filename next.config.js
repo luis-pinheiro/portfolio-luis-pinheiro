53ff6f7a2849624bb3a6356df332961814cc7683
Luis Pinheiro <profitcreations@live.com>
2021-02-04 02:12:21 +0000

    removed target on next.confid

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextTranslate = require('next-translate');
 
 module.exports = {
   // Target must be serverless
-  target: 'serverless',
+  // target: 'serverless',
   ...nextTranslate({
     webpack: (config, { isServer, webpack }) => {
       return config;

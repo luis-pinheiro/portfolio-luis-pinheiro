0bc380a749d7b2572169c8c26d98f493a1df6a92
Luis Pinheiro <profitcreations@live.com>
2021-02-15 00:25:54 +0000

    removed paralax tilt effect

--- components/CardWork.js
+++ components/CardWork.js
@@ -1,6 +1,3 @@
-import Tilt from 'react-parallax-tilt';
-import Link from 'next/link';
-
 const CardWork2 = ({ img, title, description, tags, link, linkCode }) => {
   let website = true;
   if (link === '') {
@@ -12,44 +9,45 @@ const CardWork2 = ({ img, title, description, tags, link, linkCode }) => {
     code = false;
   }
   return (
-    <Tilt className="parallax-effect" perspective={500}>
+    <div>
       <div
-        className="pb-4 overflow-hidden text-center border-transparent rounded-lg opacity-90 card-work bg-nord6 dark:bg-nord1 elevation-2 hover:elevation-7"
+        className="flex flex-col mx-auto overflow-hidden text-center border-transparent rounded-lg opacity-90 card-work bg-nord6 dark:bg-nord1 elevation-2"
         style={{
           width: '300px',
           height: '400px',
         }}
-      ></div>
-
-      <div
+      >
+        {/* <div
         className="flex flex-col justify-center pb-4 text-center card-layer inner-element"
         style={{ width: '265px', marginTop: '-377px', height: '353px', marginLeft: '-1px' }}
-      >
+      > */}
         <img className="object-cover object-center rounded-t-lg img-card" src={img} alt={title} width="300px" />
-        <h1 className="mb-3 text-xl font-medium text-nord0 dark:text-nord6 dark:text-shadow title-font ">{title}</h1>
-        <p className="mb-3 leading-relaxed text-nord1 dark:text-nord5 dark:text-shadow ">{description}</p>
-        <div className="flex flex-wrap justify-center text-center ">
-          {tags.split(' ').map((tag) => {
-            return (
-              <span
-                key={tag}
-                className="px-2 py-1 m-1 text-xs rounded bg-nord4 dark:elevation-1 dark:text-shadow dark:text-nord4 dark:bg-nord3 text-nord3 caps-all-small"
-              >
-                {tag}
-              </span>
-            );
-          })}
+        <div className="flex flex-col my-auto">
+          <h1 className="mb-3 text-xl font-medium text-nord0 dark:text-nord6 dark:text-shadow title-font ">{title}</h1>
+          <p className="mb-3 leading-relaxed text-nord1 dark:text-nord5 dark:text-shadow ">{description}</p>
+          <div className="flex flex-wrap content-around justify-center text-center ">
+            {tags.split(' ').map((tag) => {
+              return (
+                <span
+                  key={tag}
+                  className="px-2 py-1 m-1 text-xs rounded bg-nord4 dark:elevation-1 dark:text-shadow dark:text-nord4 dark:bg-nord3 text-nord3 caps-all-small"
+                >
+                  {tag}
+                </span>
+              );
+            })}
+          </div>
         </div>
-        <div className="flex flex-wrap content-end h-full text-sm">
+        <div className="flex flex-row content-around mt-auto text-base ">
           {website && (
             <a
               href={link}
               target="_blank"
               rel="noopener"
-              className="flex flex-col items-center justify-center flex-1 w-full mx-auto text-center dark:hover:text-nord6 dark:text-shadow dark:text-nord4 dark:hover:text-shadow-lg text-nord3"
+              className="flex flex-col items-center justify-center flex-1 w-full p-4 mx-auto text-center dark:hover:text-nord6 dark:text-shadow dark:text-nord4 dark:hover:text-shadow-lg text-nord3"
             >
-              <i className="w-full h-5 text-base fas fa-globe"></i>
-              <p>Website</p>
+              <i className="w-full text-lg fas fa-globe"></i>
+              <p className="">Website</p>
             </a>
           )}
 
@@ -58,22 +56,16 @@ const CardWork2 = ({ img, title, description, tags, link, linkCode }) => {
               href={linkCode}
               target="_blank"
               rel="noopener"
-              className="flex flex-col items-center justify-center flex-1 w-full mx-auto text-center dark:hover:text-nord6 dark:text-shadow dark:text-nord4 dark:hover:text-shadow-lg text-nord3"
+              className="flex flex-col items-center justify-center flex-1 w-full p-4 mx-auto text-center dark:hover:text-nord6 dark:text-shadow dark:text-nord4 dark:hover:text-shadow-lg text-nord3"
             >
-              <i className="w-full h-5 text-base fas fa-code"></i>
+              <i className="w-full text-lg fas fa-code"></i>
               <p>Source Code</p>
             </a>
           )}
-
-          <Link href="/work/OldPortfolio">
-            <a className="flex flex-col items-center justify-center flex-1 w-full mx-auto text-center dark:hover:text-nord6 dark:text-shadow dark:text-nord4 dark:hover:text-shadow-lg text-nord3">
-              <i className="w-full h-5 text-base fas fa-info"></i>
-              <p>Info</p>
-            </a>
-          </Link>
         </div>
       </div>
-    </Tilt>
+      {/* </div> */}
+    </div>
   );
 };
 
